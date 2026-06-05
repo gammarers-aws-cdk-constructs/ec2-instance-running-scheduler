6e5b66e7ffc9ffb451f9920db8af6a00b960591a
yicr <yicr@users.noreply.github.com>
2026-06-05 03:16:39 +0000

    chore(deps): update package safe-env-getter to 0.3.x

--- .projenrc.ts
+++ .projenrc.ts
@@ -19,7 +19,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     'aws-lambda-secret-fetcher@^0.3',
     'aws-sdk-client-mock@^2',
     'aws-sdk-client-mock-jest@^2',
-    'safe-env-getter@^0.2',
+    'safe-env-getter@^0.3.3',
   ],
   releaseToNpm: true,
   npmTrustedPublishing: true,
